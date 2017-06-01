e66cda10a67109e86457d049399af9167e486ce0
David Janssen <dave.janssen@me.com>
2017-06-02 01:51:32 +0200

    Added Handlebars.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,33 @@
  * Created by davidjanssen on 06.05.17.
  */
 
-(function () {
+"use strict";
 
+(function () {
+  let data = {
+    tasks: [
+      {id: "1", date: "21.06.2017", done: false, title: "CAS FEE 1", val: 4},
+      {id: "2", date: "22.06.2017", done: true, title: "CAS FEE 2", val: 1},
+      {id: "3", date: "25.06.2017", done: false, title: "CAS FEE 3", val: 2},
+      {id: "4", date: "29.06.2017", done: true, title: "CAS FEE 4", val: 4},
+      {id: "5", date: "30.06.2017", done: false, title: "CAS FEE 5", val: 5}
+    ]
+  };
+    function renderPage() {
+        let source = $('#task').html();
+        let handlebarTpl = Handlebars.compile(source);
+
+        Handlebars.registerHelper('isChecked', function (val, input) {
+          console.log(val);
+          return val === input ? 'checked' : '';
+        });
+
+        $('.tasks').html(handlebarTpl(data));
+    }
     renderPage();
 
-    function renderPage() {
-        var templateScript = $('#test').html(),
-            handlebarTpl = Handlebars.compile(templateScript),
-            context={
-                "title": "London",
-                "body": "Baker Street"
-            },
-            compiled = handlebarTpl(context);
-        $('#wrapper').html(compiled);
-    }
-
+    $("#styleswitch").on("change", () =>{
+      alert("style changed");
+      $(".wrapper").toggleClass("black");
+    });
 })();
