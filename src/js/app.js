af9710cb5fa33c129248eb5448986d140e900cd4
David Janssen <dave.janssen@me.com>
2017-06-27 23:28:30 +0200

    style-classes and data-attribute refactoring

--- src/js/app.js
+++ src/js/app.js
@@ -8,40 +8,43 @@ import {default as NoteController} from './controller/note-controller.js';
     const handlebarTpl = Handlebars.compile($('#task').html());
 
     let bindEvents = function() {
-      /// Events
-      $(".task-status").on("click", "input[name=isDone]", e => {
-        alert("guli");
+      $("body").find("[data-js-sel]").off();
+
+      $("[data-js-sel='task-status']").on("click", "input[name=isDone]", e => {
         notesObj.updateNote(e.target.dataset.id);
       });
 
-      $(".task-actions").on("click", ".toggle-task-details", e => {
+      $("[data-js-sel='task-actions']").on("click", "[data-js-sel='toggle-task-details']", e => {
         let target = $(e.currentTarget).attr("href");
         $(target).slideToggle("fast");
         $(e.currentTarget).find("i").toggleClass("fa-plus").toggleClass("fa-minus");
       });
 
-      $("#filter").on("click", "[data-sort]", e => {
-        $("#filter").find("*").removeClass("active");
-        $(e.target).addClass("active");
-        renderTpl(notesObj.sortNotes(e.target.dataset.sort, $("#toggle-completed")[0].checked));
+      $("[data-js-sel='task-filter']").on("click", "[data-js-sort]", e => {
+        let elem = $(e.target);
+        if (!elem.hasClass("active")) {
+          $(e.delegateTarget).find("*").removeClass("active");
+          $(e.target).addClass("active");
+          renderTpl(notesObj.sortNotes(e.target.dataset.jsSort, $("[data-js-sel='complete-switch']")[0].checked));
+        }
       });
 
-
-      $(".switch-wrapper")
-        .on("change", "#styleswitch",() => {
+      $("[data-js-sel='switches']")
+        .on("change", "[data-js-sel='style-switch']", e => {
           $(".wrapper-outer").toggleClass("sweet");
         })
-        .on("change", "#toggle-completed", e => {
+        .on("change", "[data-js-sel='complete-switch']", e => {
           renderTpl(notesObj.sortNotes( "", e.target.checked));
         });
-    }
-
-    let renderTpl = function(notes = notesObj.notes) {
-      $('.tasks').html(handlebarTpl({item: notes}));
-      bindEvents();
     };
 
-
+    let renderTpl = function(notes = notesObj.notes) {
+      let selector = $("[data-js-sel='tasks']");
+      selector.fadeOut( 100, () =>
+        selector.html(handlebarTpl({item: notes})).fadeIn(100, () =>
+          bindEvents())
+      )
+    };
 
     renderTpl();
 
--- src/js/detail.js
+++ src/js/detail.js
@@ -1,19 +1,23 @@
 import {default as RestService} from "./service/rest-service.js";
 import {default as Note} from "./service/note-interface.js";
 
-(function($) {
+(function ($) {
 
-  $(function() {
-      const noteService = new RestService("ls");
+  $(function () {
+    const noteService = new RestService("ls");
 
-    $("#newNote").on("submit", (e) => {
+    $("[data-js-sel='new-note']").on("click", "button[type='reset']", e => {
+      window.location.replace(e.delegateTarget.action);
+    });
+
+    $("[data-js-sel='new-note']").on("submit", e => {
       e.preventDefault();
-      let newNote =  new Note();
+      let newNote = new Note();
       newNote.creationDate = Date.now();
       newNote.dueDate = new Date($("#dateTo").val()).getTime();
-      newNote.done =  false;
-      newNote.title =  $("#title").val();
-      newNote.description =  $("#description").val();
+      newNote.done = false;
+      newNote.title = $("#title").val();
+      newNote.description = $("#description").val();
       newNote.importance = $("input[name='importance']:checked").val();
 
       // add Note
